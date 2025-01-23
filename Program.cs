c76673daa22044027e198fee5d760406d05c717b
Ângelo Vidor <angelovidor5@gmail.com>
2025-01-23 13:08:03 -0300

    first scrape from Metallum website, collecting data to explore potential ideas

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://www.metal-archives.com/bands/Summoning/29";
+
+        HttpClient client = new HttpClient();
+        string htmlContent = await client.GetStringAsync(url);
+
+        HtmlDocument doc = new HtmlDocument();
+        doc.LoadHtml(htmlContent);
+
+        var bandStatsNode = doc.DocumentNode.SelectSingleNode("//div[@id='band_stats']");
+        var bandInfoNode = doc.DocumentNode.SelectSingleNode("//div[@id='band_info']");
+
+        var terms = bandStatsNode.SelectNodes(".//dt");
+        var descriptions = bandStatsNode.SelectNodes(".//dd");
+        var bandInfo = bandInfoNode.SelectNodes(".//h1").First().InnerText.Trim();
+
+        if (terms == null || descriptions == null || bandInfo == null)
+        {
+            throw new Exception();
+        }
+
+        Console.WriteLine("Band Stats");
+        Console.WriteLine($"Band: {bandInfo}");
+        for (int i = 0; i < terms.Count; i++)
+        {
+            string term = terms[i].InnerText.Trim();
+            string description = descriptions[i].InnerText.Trim();
+            Console.WriteLine($"{term} {description}");
+        }
+    }
+}
+
+
