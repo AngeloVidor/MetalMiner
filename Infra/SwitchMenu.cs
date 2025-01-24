a4c23b8ce0fbf8a0defa3e8d9b08c9e81665ab2f
Ângelo Vidor <angelovidor5@gmail.com>
2025-01-24 15:56:36 -0300

    ForegroundColor

--- Infra/SwitchMenu.cs
+++ Infra/SwitchMenu.cs
@@ -30,33 +30,39 @@ namespace metallumscraper.Infra
 
                     Console.WriteLine("Band genre:");
                     string genre = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
 
                     if (input == 1)
                     {
                         var advanceSearcUrl = await _urlService.AdvanceSearchUrlAsync(name, genre);
                         Console.WriteLine(advanceSearcUrl);
                     }
+                    Console.ResetColor();
                     break;
                 case 2:
                     Console.WriteLine("2. Get band ID | String Response");
 
                     Console.WriteLine("Band name:");
                     name = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
                     if (input == 2)
                     {
                         long bandId = await _metallumService.GetBandIdAsync(name);
                         Console.WriteLine(bandId);
                     }
+                    Console.ResetColor();
                     break;
                 case 3:
                     Console.WriteLine("3. Get Band Profile | String Response");
                     Console.WriteLine("Band name:");
                     name = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.Green;
                     if (input == 3)
                     {
                         string profile = await _metallumService.GetBandsProfilesUrlsAsync(name);
                         Console.WriteLine(profile);
                     }
+                    Console.ResetColor();
                     break;
                 case 4:
                     Console.WriteLine("4. Extract Band ID from URL | String Response");
@@ -65,23 +71,30 @@ namespace metallumscraper.Infra
                     name = Console.ReadLine();
 
                     var url = await _metallumService.GetBandsProfilesUrlsAsync(name);
+                    Console.ForegroundColor = ConsoleColor.Green;
                     if (input == 4)
                     {
                         string extractedId = await _urlService.ExtractBandIdFromUrlAsync(url);
                         Console.WriteLine(extractedId);
                     }
+                    Console.ResetColor();
+
                     break;
                 case 5:
                     Console.WriteLine("5. Get Band Discography By Band ID | String Response");
                     Console.WriteLine("Band name:");
                     name = Console.ReadLine();
 
+
                     long band_id = await _metallumService.GetBandIdAsync(name);
                     var discography = await _metallumService.GetBandDiscographyByBandIdAsync(band_id);
+                    Console.ForegroundColor = ConsoleColor.Green;
                     foreach (var album in discography)
                     {
                         System.Console.WriteLine(album);
                     }
+                    Console.ResetColor();
+
                     break;
 
             }
