025dd23bd137646886bf2f20820e67d42d791a14
Ângelo Vidor <angelovidor5@gmail.com>
2025-01-24 16:16:28 -0300

    brinks

--- Program.cs
+++ Program.cs
@@ -18,12 +18,42 @@ class Program
         var metallumService = serviceProvider.GetRequiredService<IMetallumService>();
         var urlService = serviceProvider.GetRequiredService<IUrlService>();
 
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        string asciiArt = @"
+------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------
+███████████████████████████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+███████▀▀▀░░░░░░░▀▀▀███████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+████▀░░░░░░░░░░░░░░░░░▀████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+███│░░░░░░░░░░░░░░░░░░░│███ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██▌│░░░░░░░░░░░░░░░░░░░│▐██ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██░└┐░░░░░░░░░░░░░░░░░┌┘░██ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██░░└┐░░░░░░░░░░░░░░░┌┘░░██ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██▌░│██████▌░░░▐██████│░▐██ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+███░│▐███▀▀░░▄░░▀▀███▌│░███ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+████▄─┘██▌░░░░░░░▐██└─▄████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+███████▄░░░░░░░░░░░▄███████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+██████████▄▄▄▄▄▄▄██████████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+███████████████████████████ This program was made for study and entertainment purposes. None of the content flowing through here will be sold or monetized in any way.
+------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------";
 
+Console.WriteLine(asciiArt);
+
+        Console.WriteLine("-----------Menu------------");
         Console.WriteLine("1. Advance Search | Ajax Response");
         Console.WriteLine("2. Get Band ID | String Response");
         Console.WriteLine("3. Get Band Profile | String Response");
         Console.WriteLine("4. Extract Band ID from URL | String Response");
         Console.WriteLine("5. Get Band Discography By Band ID | String Response");
+        Console.WriteLine("-----------End------------");
+        Console.ResetColor();
 
         int input = int.Parse(Console.ReadLine());
         SwitchMenu menu = new SwitchMenu(urlService, metallumService);
